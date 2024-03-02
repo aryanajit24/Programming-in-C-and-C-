b3fe614348e1ba5a041de73a10cae7144feea9f7
Aryan Ajit Panicker <70631332+aryanajit24@users.noreply.github.com>
2024-03-03 00:54:22 +0400

    Add files via upload

---
+++ Assignment 1/a1_p1.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main() {
+    double results;
+    results = (double)(3+1)/5; /*the code did not work without the double because the answer was a fraction
+    but if you add double before the equation it changes the fraction to decimal */
+    printf ("the value of 4/5 is %1f \n", results);
+
+return 0;
+}
---
+++ Assignment 1/a1_p3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    CH-230-A
+    a1 p3.[c or cpp or h]
+    Jaydev Dhanji
+    jdhanji@jacobs-university.de 
+    */
+    //question 3
+    float result; /* The result of the division */
+    int a = 5;
+    int b = 13.5;
+    result = a / b;
+    printf("The result is %f\n", result);
+    /*int values can not have decimal points if they do they will be counted as float numbers and not integers, also the
+    formatting specifications for floating point is %f and not %d */
+
+    return 0;
+}
